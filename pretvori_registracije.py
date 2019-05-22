cf919dae7586c4393168cfde7cb6b3082bf7768d
andrej <andrej.borstnik@matheo.si>
2019-05-22 15:41:19 +0200

    SSOL 4

--- pretvori_registracije.py
+++ pretvori_registracije.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 
-with open("Registracije/registracije3.csvreg","r",encoding="utf-8") as f:
-    with open("Registracije/registracije3.csv","w",encoding="utf-8") as g:
+with open("Registracije/registracije4.csvreg","r",encoding="utf-8") as f:
+    with open("Registracije/registracije4.csv","w",encoding="utf-8") as g:
         a = f.read()
         b = a.split("\n")
         ozs = urlopen("http://www.orientacijska-zveza.si/index.php?id=56")
